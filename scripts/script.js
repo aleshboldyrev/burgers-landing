852e43bfa15559a3aa054df7e6fababf150a1125
Aleksey Boldyrev <dublonalex2019@gmail.com>
2023-08-18 22:30:47 +0400

    add scripts

---
+++ scripts/script.js
@@ -0,0 +1,72 @@
+// обработчик событий по клику на кнопку "Смотреть меню".
+document.getElementById("main-action-button").onclick = function () {
+    document.getElementById("products").scrollIntoView({behavior: "smooth"});
+}
+
+// Обработчик событий для ссылок: Почему у нас - Меню бургеров - Оформление заказа
+let links = document.querySelectorAll(".menu-item > a");
+for (let i = 0; i < links.length; i++) {
+    links[i].onclick = function () {
+        document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior: "smooth"});
+    }
+}
+
+// Обработчик событий для карточек меню
+let buttons = document.getElementsByClassName("product-button");
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        document.getElementById("order").scrollIntoView({behavior: "smooth"});
+    }
+}
+
+// Сохраняем элементы инпута в переменные
+let burger = document.getElementById("burger");
+let name = document.getElementById("name");
+let phone = document.getElementById("phone");
+
+// Обработчик событий для кнопки в инпуте(валидация полей ввода)
+document.getElementById("order-action").onclick = function () {
+    let hasError = false;
+// Меняем фон инпута если поле пустое
+    [burger, name, phone].forEach(item => {
+        if (!item.value) {
+            item.parentElement.style.background = "red";
+            hasError = true;
+        } else {
+            item.parentElement.style.background = "";
+        }
+    });
+
+// Обработка результата выполнения валидации
+    if (!hasError) {
+        [burger, name, phone].forEach(item => {
+          item.value = "";
+        });
+        alert("Спасибо за заказ! Мы скоро свяжемся с вами!");
+    }
+}
+
+// Переключение валют
+let prices = document.getElementsByClassName("products-item-price");
+
+document.getElementById("change-currency").onclick = function (e) {
+    let currentCurrency = e.target.innerText;
+
+    let newCurrency = "$";
+    let coefficient = 1;
+
+    if (currentCurrency === "$") {
+        newCurrency = "₽";
+        coefficient = 90;
+    } else if (currentCurrency === "₽") {
+        newCurrency = "BYN";
+        coefficient = 3;
+    }
+
+    e.target.innerText = newCurrency;
+
+    for (let i = 0; i < prices.length; i++) {
+        prices[i].innerText = +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
+
+    }
+}
